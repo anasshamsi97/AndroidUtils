9f004b342e52feedce0b728ed592fc608e146ac9
Muhammad Anas <anasshamsi97@gmail.com>
2019-02-14 16:04:52 +0500

    added snackbar,vibrate,simcard, etc utils

--- app/src/main/java/com/example/myutils/MainActivity.java
+++ app/src/main/java/com/example/myutils/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.myutils;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+
+import com.example.myutils.Utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View parentLayout = findViewById(android.R.id.content);
+        Utils.showIndefiniteSnackBarWithAction(parentLayout, "Hello World");
+        Utils.showSnackBar(parentLayout,"Hello World");
+        boolean isTablet = Utils.isTablet();
+        Log.d(TAG, "onCreate: "+isTablet);
+        Utils.vibrate(500,this);
     }
 }
--- app/src/main/java/com/example/myutils/Utils/Utils.java
+++ app/src/main/java/com/example/myutils/Utils/Utils.java
@@ -2,10 +2,26 @@ package com.example.myutils.Utils;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.design.widget.Snackbar;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,29 +32,32 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 /**
  * Created by Muhammad Anas Shamsi on 2/13/19.
  */
+@SuppressLint("MissingPermission")
 public class Utils {
 
     private final static String TAG = "Utils";
 
     public static boolean isInternetConnected(Context context) {
         ConnectivityManager cm =
-                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm != null ? cm.getActiveNetworkInfo() : null;
         return activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
     }
 
-    public static SweetAlertDialog showSweetLoader(Context context, int dialogtype, String title) {
-        SweetAlertDialog pDialog = new SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE);
-        pDialog.getProgressHelper().setBarColor(Color.parseColor("#5C6BC0"));
-        pDialog.setTitleText(title);
-        pDialog.setCancelable(false);
-        pDialog.show();
+    public static SweetAlertDialog showSweetLoader(Context context, String title) {
+        SweetAlertDialog sweetAlertDialog = new SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE);
+        sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#5C6BC0"));
+        sweetAlertDialog.setTitleText(title);
+        sweetAlertDialog.setCancelable(false);
+        sweetAlertDialog.show();
 
-        return pDialog;
+        return sweetAlertDialog;
     }
 
     public static String getDateTime() {
@@ -47,7 +66,12 @@ public class Utils {
         return sdf.format(c.getTime());
     }
 
-    @SuppressLint("MissingPermission")
+    /**
+     * Location.
+     * <p>Must hold {@code <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />}</p>
+     * OR
+     * <p>Must hold {@code <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />}</p>
+     */
     public static void getLocation(Context context, final CallbackLocation callbackLocation) {
         LocationRequest request = new LocationRequest();
         request.setNumUpdates(1);
@@ -76,13 +100,73 @@ public class Utils {
                 }.getType());
         try {
             JSONArray jsonArray = new JSONArray(element);
-            Log.d(TAG, "toJSONArray: "+jsonArray.toString());
+            Log.d(TAG, "toJSONArray: " + jsonArray.toString());
         } catch (JSONException e) {
-            Log.d(TAG, "toJSONArray: "+e.getMessage());
+            Log.d(TAG, "toJSONArray: " + e.getMessage());
         }
+    }
+
+    public static void showIndefiniteSnackBarWithAction(View parentLayout, String s) {
+        Snackbar.make(parentLayout, s, Snackbar.LENGTH_INDEFINITE)
+                .setAction("CLOSE", null)
+                .setActionTextColor(Resources.getSystem().getColor(android.R.color.holo_red_light))
+                .show();
+    }
+
+    public static void showSnackBar(View parentLayout, String s) {
+        Snackbar.make(parentLayout, s, Snackbar.LENGTH_LONG).show();
+    }
+
+    private static boolean checkSimAvailability(Context context) {
+        TelephonyManager telMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        int simState = 0;
+        if (telMgr != null)
+            simState = telMgr.getSimState();
+
+        switch (simState) {
+            case TelephonyManager.SIM_STATE_ABSENT:
+                return false;
+            case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
+                // do something
+                break;
+            case TelephonyManager.SIM_STATE_PIN_REQUIRED:
+                // do something
+                break;
+            case TelephonyManager.SIM_STATE_PUK_REQUIRED:
+                // do something
+                break;
+            case TelephonyManager.SIM_STATE_READY:
+                // do something
+                break;
+            case TelephonyManager.SIM_STATE_UNKNOWN:
+                // do something
+                break;
+        }
+        return true;
     }
 
     public interface CallbackLocation {
         void locationResult(String[] latlng);
     }
+
+    public static boolean isTablet() {
+        return (Resources.getSystem().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+    /**
+     * Vibrate.
+     * <p>Must hold {@code <uses-permission android:name="android.permission.VIBRATE" />}</p>
+     */
+    public static void vibrate(int milliSeconds, Context context) {
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(milliSeconds, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(milliSeconds);
+        }
+    }
+
 }
